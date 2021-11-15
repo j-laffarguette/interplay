735d5357c6433db0e07e331602d4790293a29dc8
Julien <j-laffarguette@o-lambret.fr>
2021-11-15 19:43:06 +0100

    Update interplay.py
    doing class

--- interplay.py
+++ interplay.py
@@ -5,86 +5,135 @@ from math import pi, sin
 import cv2
 import SimpleITK as sitk
 
-mpl.use('Qt5Agg')
 
+class Dose:
+    def __init__(self, num_beams, frequency, dose_waited, dose_rate):
+        # sizes of itv and gtv
+        self.height = 250
+        self.ratio = 2
+        self.tot_length = self.height * self.ratio
 
-def positions(t0=0, tn=60, step=0.01, f=6, pn=100, to_plot=True):
-    omega = 2 * pi * f  # Pulsation
-    time = np.arange(t0, tn, omega * step)  # Time values for sin
-    position = pn / 2 * np.sin(time + pi / 2) + pn / 2  # Positions along the time
-    if to_plot:
-        plt.plot(time, position)
-        plt.xlabel('temps (s)')
-        plt.show()
+        # initialization of itv and gtv
+        self.s_itv = [self.height, self.height * self.ratio]
+        self.s_gtv = [self.height, self.height]
+        self.itv = np.zeros(self.s_itv)
+        self.gtv = np.ones(self.s_gtv)
 
-    return position, time
+        # Time values
+        self.freq = frequency  # Hz
+        self.tn = 1000  # End of calculation (s)
+        self.step = 0.001
 
+        self.p0, self.pn = 0, self.s_itv[1] - self.s_itv[0]  # Available positions for gtv in itv (eg : from 0 to 100)
+        self.positions = []
+        self.time = []
+        self.get_positions()
 
-################################
-# DEFINITIONS
-################################
+        # dose matrix
+        self.dose = np.zeros(self.s_gtv)
+        self.dose_rate = dose_rate
+        self.dose_needed = dose_waited
 
-# Time values
-t_tot = 25  # seconds
-freq = 10
-step = 0.0001
+        # Beams
+        self.num_beams = num_beams
+        self.b_size1, self.b_size2 = (self.s_itv[0], int(round(self.s_itv[1] / self.num_beams)))
+        self.beam_position_in_time = []
+        self.time_needed = 0
+        self.do_beams()
 
-# sizes of itv and gtv
-height = 250
-ratio = 2
-tot_length = height * ratio
+    def get_positions(self, sinus = False, to_plot=True):
+        t0 = 0
+        omega = 2 * pi * self.freq  # Pulsation
+        self.time = np.arange(t0, self.tn, omega * self.step)  # Time values for sin
 
-s_itv = [height, height * ratio]
-s_gtv = [height, height]
-p0, pn = 0, s_itv[1] - s_itv[0]  # Available positions for gtv in itv (eg : from 0 to 100)
-positions, time = positions(tn=t_tot, step=0.001, f=freq, pn=pn, to_plot=False)
+        if sinus:
+            self.positions = self.pn / 2 * np.sin(self.time + pi / 2) + self.pn / 2  # Positions along the time
+        else:
+            self.positions = self.pn* np.sin(self.time + pi / 2)**4
 
-# initialization of itv and gtv
-itv = np.zeros(s_itv)
-gtv = np.ones(s_gtv)
+        if to_plot:
+            plt.plot(self.time[0:1000], self.positions[0:1000])
+            plt.xlabel('temps (s)')
+            plt.show()
 
-# Beams
-num_beams = 5
-b_size1, b_size2 = (s_itv[0], int(round(s_itv[1] / num_beams)))
-beam = np.ones([b_size1, b_size2])
-beam_positions = range(0,num_beams*b_size2,b_size2)
+    def do_beams(self):
+        self.time_needed = self.dose_needed / self.dose_rate * 60
+        beam = np.ones([self.b_size1, self.b_size2])
+        beam_positions = range(0, self.num_beams * self.b_size2, self.b_size2)
 
+        for b in range(self.num_beams):
+            self.beam_position_in_time += [beam_positions[b] for t in self.time if
+                                           int(t) in range(int(b * self.time_needed), int(self.time_needed * (b + 1)))]
+        print()
 
-beam_position_in_time = []
-for b in range(num_beams):
-    beam_position_in_time += [beam_positions[b] for t in time if
-                              int(t) in range(int(b * t_tot / num_beams), int((b + 1) * t_tot / num_beams))]
+    def main_loop(self):
+        index = 0
+        for i, t, bp in zip(self.positions, self.time, self.beam_position_in_time):
+            index += 1
 
-# dose matrix
-dose = np.zeros(s_gtv)
+            # New temporary area (size of itv)
+            i = int(i)  # one needs integer values
+            iitv = np.copy(self.itv)
+            iitv_to_show = np.copy(self.itv)
+            iitv[0:self.s_gtv[0], i:i + self.s_gtv[1]] = self.gtv
+            iitv_to_show[0:self.s_gtv[0], i:i + self.s_gtv[1]] = self.dose / 255
 
-# dose rate
-d = 4  # Gy/min
+            # intersection between iitv and beam
+            temp_beam = np.zeros_like(iitv)
+            temp_beam[0:self.s_itv[0], bp:bp + self.b_size2] = 1
+            intersection = temp_beam[0:self.s_itv[0], i:i + self.s_gtv[1]]
+
+            # dose accumulation
+            self.dose = self.dose + intersection
+            ind = np.unravel_index(np.argmax(self.dose, axis=None), self.dose.shape)
+
+            cv2.rectangle(iitv_to_show, (i + self.s_gtv[1], self.s_gtv[0]), (i, 0), (255, 255, 0), 2)
+            image = cv2.copyMakeBorder(iitv_to_show, 100, 100, 50, 50, cv2.BORDER_CONSTANT, None, value=0.4)
+            cv2.imshow("gtv", image)
+
+            # cv2.imshow('dose', dose/np.max(dose))
+
+            cv2.imshow('beam', temp_beam)
+
+            k = cv2.waitKey(int(20))
+            if index == len(self.beam_position_in_time):
+                cv2.waitKey(0)
+            if k == 27:
+                break
+
 
 if __name__ == "__main__":
-    cv2.namedWindow("gtv")
-    # cv2.namedWindow("dose")
-    cv2.namedWindow("beam")
+    num_beams = 8
+    frequency = 6
+    dose_waited = 2
+    dose_rate = 20
 
-    for i, t, bp in zip(positions, time, beam_position_in_time):
+    d = Dose(num_beams, frequency, dose_waited, dose_rate)
+    d.main_loop()
 
-        # New temporary area (size of itv)
-        i = int(i)  # one needs integer values
-        iitv = np.copy(itv)
-        iitv_to_show = np.copy(itv)
-        iitv[0:s_gtv[0], i:i + s_gtv[1]] = gtv
-        iitv_to_show[0:s_gtv[0], i:i + s_gtv[1]] = dose / 255
-
-        # intersection between iitv and beam
-        temp_beam = np.zeros_like(iitv)
-        temp_beam[0:s_itv[0], bp:bp + b_size2] = 1
-        intersection = temp_beam[0:s_itv[0], i:i + s_gtv[1]]
-
-        # dose accumulation
-        dose = dose + intersection
-        ind = np.unravel_index(np.argmax(dose, axis=None), dose.shape)
-
-        # DOSE IMAGE
+    # cv2.namedWindow("gtv")
+    # # cv2.namedWindow("dose")
+    # cv2.namedWindow("beam")
+    # #
+    # for i, t, bp in zip(positions, time, beam_position_in_time):
+    #
+    #     # New temporary area (size of itv)
+    #     i = int(i)  # one needs integer values
+    #     iitv = np.copy(itv)
+    #     iitv_to_show = np.copy(itv)
+    #     iitv[0:s_gtv[0], i:i + s_gtv[1]] = gtv
+    #     iitv_to_show[0:s_gtv[0], i:i + s_gtv[1]] = dose / 255
+    #
+    #     # intersection between iitv and beam
+    #     temp_beam = np.zeros_like(iitv)
+    #     temp_beam[0:s_itv[0], bp:bp + b_size2] = 1
+    #     intersection = temp_beam[0:s_itv[0], i:i + s_gtv[1]]
+    #
+    #     # dose accumulation
+    #     dose = dose + intersection
+    #     ind = np.unravel_index(np.argmax(dose, axis=None), dose.shape)
+    #
+    #     # DOSE IMAGE
         # dose_to_display = dose
         # ind = np.unravel_index(np.argmax(dose, axis=None), dose.shape)
         # cv2.imwrite('dose.png', dose_to_display)
@@ -98,18 +147,5 @@ if __name__ == "__main__":
         # cv2.putText(gray_three, f"{np.max(dose)}", (int(ind[1]) + 10, int(s_gtv[0] / 2) + 5), cv2.FONT_HERSHEY_SIMPLEX, 1,
         #             (255, 255, 255), 1)
         # cv2.imshow('dose', gray_three)
-
-        cv2.rectangle(iitv_to_show, (i + s_gtv[1], s_gtv[0]), (i, 0), (255, 255, 0), 2)
-        image = cv2.copyMakeBorder(iitv_to_show, 100, 100, 50, 50, cv2.BORDER_CONSTANT, None, value=0.4)
-        cv2.imshow("gtv", image)
-
-        # cv2.imshow('dose', dose/np.max(dose))
-
-        cv2.imshow('beam', temp_beam)
-
-        k = cv2.waitKey(int(20))
-        if t == max(time):
-            cv2.waitKey(0)
-        if k == 27:
-            break
-
+    #
+    #
